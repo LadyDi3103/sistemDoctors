3a385b2632c8986a943941de9c69d2aec33d514c
LadyDi3103 <chinamy2004@gmail.com>
2024-02-01 20:43:24 -0500

    UPDATE:FORM to create new patient

--- src/app/views/dashboard/crea-paciente/crea-paciente.component.ts
+++ src/app/views/dashboard/crea-paciente/crea-paciente.component.ts
@@ -1,61 +1,68 @@
-import { Component, Inject } from '@angular/core';
-import { FormBuilder, Validators } from '@angular/forms'
+import { Component, Inject, OnInit } from '@angular/core';
+import { FormBuilder,FormGroup, Validators } from '@angular/forms'
 import { PacientesService } from '../../../services/pacientes/pacientes.service';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
-
-
-
+import { formatDate } from '@angular/common';
 
 @Component({
   selector: 'app-crea-paciente',
   templateUrl: './crea-paciente.component.html',
   styleUrls: ['./crea-paciente.component.css']
 })
-export class CreaPacienteComponent {
+export class CreaPacienteComponent implements OnInit {
 
   pacientes:any[]=[];
+  form!: FormGroup;
 
-form = this.fb.group({
-paciente: ['', [
-Validators.required,
-Validators.minLength(5),
-Validators.maxLength(60),
-]],
-edad:[],
-appointment: [new Date()],
-genderType: [''],
-IdTipoDocumento: [''],
-NumeroDocumento: [''],
-Cel:[],
-Email:[''],
-FNac:[],
-Hijos:[],
-Domicilio:[''],
-Ocupac:[],
-Gpo:[],
-EC:[],
-alergias:[],
-MEN:[],
-SÑO:[],
-Cirugias:[],
-CPO:[],
-NOC:[],
-AntFam:[],
-ANS:[],
-CIG:[],
-AntPers:[],
-})
+  constructor(
+    private fb: FormBuilder, 
+    private pacientesService: PacientesService,
+    public dialogRef: MatDialogRef<CreaPacienteComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: any
+    ){}
 
+    ngOnInit() {
+      this.initializeForm();
+    }
+  initializeForm() {
+    const currentDate = new Date();
+    const formattedDate = formatDate(currentDate, 'dd/MM/yyyy', 'en-US');
 
+    this.form = this.fb.group({
+      paciente: ['', [Validators.required, Validators.minLength(5), Validators.maxLength(60)]],
+      edad: [],
+      appointment: [formattedDate, Validators.required],
+      genderType: [''],
+      IdTipoDocumento: [''],
+      NumeroDocumento: [''],
+      Cel: [],
+      Email: [''],
+      FNac: [],
+      Hijos: [],
+      Domicilio: [''],
+      Ocupac: [],
+      Gpo: [],
+      EC: [],
+      alergias: [],
+      MEN: [],
+      SÑO: [],
+      Cirugias: [],
+      CPO: [],
+      NOC: [],
+      AntFam: [],
+      ANS: [],
+      CIG: [],
+      AntPers: []
+    });
+  }
 
-constructor(
-  private fb: FormBuilder, 
-  private pacientesService: PacientesService,
-  public dialogRef: MatDialogRef<CreaPacienteComponent>,
-  @Inject(MAT_DIALOG_DATA) public data: any){}
+  // Agrega este getter para obtener el control del nombre
+  get nombrePaciente() {
+    return this.form ? this.form.get('paciente') : null;
+  }
 
 crearPacienteNuevo(){
-  if(this.form.valid){
+  if(this.form && this.form.valid){
     const body = this.form.value;
     console.log(body, 'body');
     
@@ -73,13 +80,15 @@ crearPacienteNuevo(){
       console.error('Formulario no válido');
     }
   }
-
-get nombrePaciente(){
-  return this.form.controls['paciente'];
-}
-
   onCancelClick() {
-    // Restablecer los valores del formulario a su estado inicial
+    this.resetForm();
+  }
+
+  cancelar(): void {
+    this.dialogRef.close();
+  }
+
+  private resetForm() {
     this.form.reset({
       paciente: '',
       appointment: new Date(),
@@ -87,15 +96,10 @@ get nombrePaciente(){
       // ... (resto de valores iniciales)
     });
 
-    // También puedes marcar todos los controles como no tocados para limpiar los mensajes de validación
     Object.keys(this.form.controls).forEach(controlName => {
       this.form.get(controlName)?.markAsUntouched();
     });
   }
-  cancelar(): void {
-    this.dialogRef.close();
-  }
-
 }
 
 
